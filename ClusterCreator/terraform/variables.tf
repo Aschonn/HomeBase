ca213535e74dd3fc5a9bfef024c808de5efe50b5
aschonn <andrew.schonnesen@gmail.com>
2025-11-17 22:11:22 +0000

    addng create cluster scripts

---
+++ ClusterCreator/terraform/variables.tf
@@ -0,0 +1,9 @@
+locals {
+    proxmox_host   = "192.168.0.100"
+    proxmox_node   = "pve"
+    template_vm_id = 9000
+#     unifi_api_url  = "https://10.0.0.1/"
+#     minio_endpoint = "https://s3.christensencloud.us"
+#     minio_region   = "default"
+#     minio_bucket   = "terraform-state"
+}
